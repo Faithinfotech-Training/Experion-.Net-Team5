520edb85766e3e9e5f807952cf3a6439eeddcd28
Rohit Deepak <rohit.deepak@experionglobal.com>
2021-11-25 21:31:09 +0530

    git testing

--- CMSApi/Startup.cs
+++ CMSApi/Startup.cs
@@ -30,6 +30,7 @@ namespace CMSApi
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         //git comment
+        //testing
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
