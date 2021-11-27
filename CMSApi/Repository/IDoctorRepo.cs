1499858535c1f9ed82a2766fd59cd66dbf90a6fb
shallet.eldho <shallet.eldho@experionglobal.com>
2021-11-27 10:25:02 +0530

    patientdata added

---
+++ CMSApi/Repository/IDoctorRepo.cs
@@ -0,0 +1,20 @@
+﻿using CMSApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMSApi.Repository
+{
+   public interface IDoctorRepo
+    {
+        //Asynchronous operation
+        Task<List<Doctors>> GetDoctors();
+
+        //add doctor
+        Task<int> AddDoctor(Doctors doc);
+
+        //update doctor
+        Task UpdateDoctor(Doctors doc);
+    }
+}
---
+++ CMSApi/Repository/Staff.cs
@@ -0,0 +1,75 @@
+﻿using CMSApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMSApi.Repository
+{
+    public class Staff : IStaff
+    {
+        DBClinicContext _db;
+        public Staff(DBClinicContext db)
+        {
+            _db = db;
+        }
+        //view staff
+        public async Task<List<Staffs>> GetStaff()
+        {
+            if (_db != null)
+            {
+                return await _db.Staffs.ToListAsync();
+            }
+            return null;
+
+
+
+        }
+        //add Staff
+        #region
+        public async Task<Staffs> Addstaff(Staffs staff)
+        {
+            //--- member function to add department ---//
+            if (_db != null)
+            {
+                await _db.Staffs.AddAsync(staff);
+                await _db.SaveChangesAsync();
+                return staff;
+            }
+            return null;
+
+        }
+
+        #endregion
+
+        
+
+       
+
+       
+        //update staff
+        public async Task<Staffs> UpdateStaff(Staffs staff)
+        {
+            if (_db != null)
+            {
+                _db.Staffs.Update(staff);
+                await _db.SaveChangesAsync();
+                return staff;
+            }
+            return null;
+        }
+        //--- member function to add values to room table ---//
+
+
+       
+
+
+
+
+
+
+    }
+}
+
+
