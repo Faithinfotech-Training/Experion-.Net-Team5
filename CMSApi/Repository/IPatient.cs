508475fb1b71c116f1321444ccf3b9f4dd334c0f
Rohit Deepak <rohit.deepak@experionglobal.com>
2021-11-27 10:40:49 +0530

    Added IPatient

--- CMSApi/Repository/IPatient.cs
+++ CMSApi/Repository/IPatient.cs
@@ -8,6 +8,16 @@ namespace CMSApi.Repository
 {
     public interface IPatient
     {
-        
+        //get all patients
+        Task<List<Patients>> GetAllPatients();
+
+        //get patient by Id
+
+
+        //add patient
+        Task<Patients> AddPatients(Patients patient);
+
+        //update patient
+        Task<Patients> UpdatePatients(Patients patient);
     }
 }
