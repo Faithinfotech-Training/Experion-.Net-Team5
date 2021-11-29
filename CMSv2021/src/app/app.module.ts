28ae2338aebe543e83a044361a0fd829df21f563
Rohit Deepak <rohit.deepak@experionglobal.com>
2021-11-29 10:35:54 +0530

    added files

--- CMSv2021/src/app/app.module.ts
+++ CMSv2021/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StaffComponent } from './staff/staff.component';
+import { AdminComponent } from './admin/admin.component';
+import { PatientsComponent } from './patients/patients.component';
+import { PatientComponent } from './patients/patient/patient.component';
+import { PatientListComponent } from './patients/patient-list/patient-list.component';
+import { StaffsComponent } from './staffs/staffs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StaffComponent,
+    AdminComponent,
+    PatientsComponent,
+    PatientComponent,
+    PatientListComponent,
+    StaffsComponent
   ],
   imports: [
     BrowserModule,
