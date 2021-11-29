3c588fd88bcf03b671c36606978ed6dad8732c32
Rohit Deepak <rohit.deepak@experionglobal.com>
2021-11-29 13:57:52 +0530

    Added staff

--- CMSv2021/src/app/app-routing.module.ts
+++ CMSv2021/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { StaffComponent } from './staffs/staff/staff.component';
+import { StaffListComponent } from './staffs/staff-list/staff-list.component';
 
 const routes: Routes = [
   {path:'',redirectTo:"/login",pathMatch:'full'},
-  {path:'login',component:LoginComponent}
+  {path:'login',component:LoginComponent},
+  { path: 'staff', component: StaffComponent },
+  { path: 'stafflist', component: StaffListComponent },
+  { path: 'staff/:staffId', component: StaffComponent },
 ];
 
 @NgModule({
