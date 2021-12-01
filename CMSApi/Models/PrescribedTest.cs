00730c8f9015468acdbca87940d93682129b47f6
Rohit Deepak <rohit.deepak@experionglobal.com>
2021-12-01 18:03:23 +0530

    ViewModel

---
+++ CMSApi/Models/PrescribedTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CMSApi.Models
+{
+    public partial class PrescribedTest
+    {
+        public int PrescribedTestId { get; set; }
+        public string PrescribedTestName { get; set; }
+        public bool? IsActive { get; set; }
+        public int? TestId { get; set; }
+        public int? PatientId { get; set; }
+
+        public virtual Patients Patient { get; set; }
+        public virtual Tests Test { get; set; }
+    }
+}
